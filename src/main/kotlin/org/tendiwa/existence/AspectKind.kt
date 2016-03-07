028e384d784e7c0add5b9880af0590fbf5bd5d67
Georgy Vlasov <wlasowegor@gmail.com>
2016-03-07 19:11:47 +0500

    Make AspectKind not a data class

--- src/main/kotlin/org/tendiwa/existence/AspectKind.kt
+++ src/main/kotlin/org/tendiwa/existence/AspectKind.kt
@@ -2,6 +2,6 @@ package org.tendiwa.existence
 
 import org.tendiwa.stimuli.StimulusKind
 
-data class AspectKind(val stimuli: List<StimulusKind>)
+class AspectKind(val stimuli: List<StimulusKind>)
 
 fun NoStimuliAspectKind() = AspectKind(emptyList())
