2b49cdb3d428ba2596eac7beefd7ce25f9cfe2d0
Georgy Vlasov <wlasowegor@gmail.com>
2016-03-08 22:45:49 +0500

    Implement subscribing particular subscribers to all events in StimulusMedium

--- src/main/kotlin/org/tendiwa/stimuli/StimulusMedium.kt
+++ src/main/kotlin/org/tendiwa/stimuli/StimulusMedium.kt
@@ -12,6 +12,7 @@ class StimulusMedium {
         LinkedHashMap()
 
     private val providerOfSets = { LinkedHashSet<Aspect>() }
+    private val omnisubscribers: MutableList<(Stimulus) -> Unit> = ArrayList()
 
     fun subscribe(subscriber: Aspect, stimulusKind: StimulusKind) {
         subscriptions
@@ -19,7 +20,13 @@ class StimulusMedium {
             .add(subscriber)
     }
 
+    fun subscribeToAll(omnisubcriber: (Stimulus) -> Unit) {
+        omnisubscribers.add(omnisubcriber)
+    }
+
     fun sendStimulus(stimulus: Stimulus) {
+        omnisubscribers
+            .forEach { it(stimulus) }
         subscriptions
             .getOrPut(stimulus.kind, providerOfSets)
             .forEach { it.reaction(this, stimulus) }
